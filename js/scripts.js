b907aa0fb725e469970ddee306d6300ce3c8e2ac
Dyanna Castro <dyannacastro04@gmail.com>
2024-09-25 03:13:00 +0800

    final changes

--- js/scripts.js
+++ js/scripts.js
@@ -14,6 +14,117 @@ window.addEventListener('DOMContentLoaded', event => {
 
     };
 
+    // Select all the timeline panels
+  const timelinePanels = document.querySelectorAll('.timeline-panel');
+
+  const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('show'); // Add the 'show' class when visible
+        observer.unobserve(entry.target); // Stop observing once it's shown
+      }
+    });
+  });
+
+  // Observe each panel
+  timelinePanels.forEach(panel => {
+    observer.observe(panel);
+  });
+  
+  particlesJS("particles-js", {
+    "particles": {
+      "number": {
+        "value": 80, // You can adjust this number
+        "density": {
+          "enable": true,
+          "value_area": 800
+        }
+      },
+      "color": {
+        "value": "#B0D9B1"
+      },
+      "shape": {
+        "type": "circle",
+        "stroke": {
+          "width": 0,
+          "color": "#000000"
+        },
+        "polygon": {
+          "nb_sides": 5
+        }
+      },
+      "opacity": {
+        "value": 0.5,
+        "random": false
+      },
+      "size": {
+        "value": 3,
+        "random": true
+      },
+      "line_linked": {
+        "enable": true,
+        "distance": 150,
+        "color": "#B0D9B1",
+        "opacity": 0.4,
+        "width": 1
+      },
+      "move": {
+        "enable": true,
+        "speed": 6,
+        "direction": "none",
+        "random": false,
+        "straight": false,
+        "out_mode": "out",
+        "bounce": false,
+        "attract": {
+          "enable": false,
+          "rotateX": 600,
+          "rotateY": 1200
+        }
+      }
+    },
+    "interactivity": {
+      "detect_on": "canvas",
+      "events": {
+        "onhover": {
+          "enable": true,
+          "mode": "repulse"
+        },
+        "onclick": {
+          "enable": true,
+          "mode": "push"
+        },
+        "resize": true
+      },
+      "modes": {
+        "grab": {
+          "distance": 400,
+          "line_linked": {
+            "opacity": 1
+          }
+        },
+        "bubble": {
+          "distance": 400,
+          "size": 40,
+          "duration": 2,
+          "opacity": 8,
+          "speed": 3
+        },
+        "repulse": {
+          "distance": 200,
+          "duration": 0.4
+        },
+        "push": {
+          "particles_nb": 4
+        },
+        "remove": {
+          "particles_nb": 2
+        }
+      }
+    },
+    "retina_detect": true
+  });
+  
     // Shrink the navbar 
     navbarShrink();
 
